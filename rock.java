d9e15813e73e13a1f15efe70705e80031270ef51
ElwoodHogan <73083286+ElwoodHogan@users.noreply.github.com>
2020-10-18 17:59:38 -0500

    Add files via upload

---
+++ rock.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class rock here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class rock extends Environment
+{
+    /**
+     * Act - do whatever the rock wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
