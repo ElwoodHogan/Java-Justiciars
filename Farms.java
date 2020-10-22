3f9b173e3b11cfac8d43adbdc858ee5e3843ce89
ElwoodHogan <elh0061@auburn.edu>
2020-10-22 14:17:24 -0500

    Added farms menu
    farms

--- Farms.java
+++ Farms.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Buildings here.
+ * Write a description of class Farms here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Buildings extends Tiles
+public class Farms extends Tiles
 {
     /**
-     * Act - do whatever the Buildings wants to do. This method is called whenever
+     * Act - do whatever the Farms wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
--- Tiles.java
+++ Tiles.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Buildings here.
+ * Write a description of class Tiles here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Buildings extends Tiles
+public class Tiles extends Actor
 {
     /**
-     * Act - do whatever the Buildings wants to do. This method is called whenever
+     * Act - do whatever the Tiles wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
