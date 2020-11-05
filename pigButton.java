7d6727f3183ac67dcc2c7e50f531f666ab0e716f
marisakelley <73195899+marisakelley@users.noreply.github.com>
2020-11-05 14:20:19 -0600

    pig button

---
+++ pigButton.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class pigButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class pigButton extends AnimalButtons
+{
+    //Edit these fields
+    private final String tileName = "Pig";
+    public double cost = 12;
+    
+    public void act() 
+    {
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        getWorld().showText(tileName, getX(), getY()-44);
+        getWorld().showText(cost + "$", getX(), getY()+39);
+        //detects if it was clicked, and if so, spawns a blueprint for the pig on the mouse's location
+        if (Greenfoot.mouseClicked(this) && ((MainWorld)getWorld()).money > cost) {
+                ((MainWorld)getWorld()).addObject(new pigTile(cost), mouse.getX(), mouse.getY());
+            }
+    }   
+}
