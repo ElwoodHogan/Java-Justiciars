956200273d470648fcda671267e056f473054f97
Tullis Nelson <tullisnelson@gmail.com>
2020-11-05 12:42:38 -0600

    LampPost

---
+++ LampPost.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class LampPost here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LampPost extends decorButton
+{
+    /**
+     * Act - do whatever the LampPost wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
